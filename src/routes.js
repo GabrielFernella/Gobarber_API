61e1108c9d5f5d0074795f706033299f99079613
Gabriel Fernella Garcia dos Santos <GabrielFernella@users.noreply.github.com>
2020-03-30 16:13:39 -0300

    Created First User

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
-import { Router} from 'express'; // Forma de importar o Router para o APP.js
+import { Router } from 'express'; // Forma de importar o Router para o APP.js
+
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({message: 'Hello World teste '})
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Gabs',
+        email: 'gabs@gmail.com',
+        password_hash: '123456'
+    })
+
+    return res.json(user);
 })
 
 
