f8a4742636cbaab3d718b17a9458bc2c45e20ec1
Gabriel Fernella Garcia dos Santos <GabrielFernella@users.noreply.github.com>
2020-04-06 16:47:49 -0300

    Send Mail and Create Template fot SendMail
    created Function sendMail with template using handlebars

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '0f45740b299969',
+        pass: 'd36bc17991fc10',
+    },
+    default: {
+        from: 'Equipe Gobarber <noreplay@gobarber.com>'
+    }
+};
